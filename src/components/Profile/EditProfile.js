03705cc67da41cc934d3e17972ba95ea2dd9a0f2
Olga Fomin <olga.fomin26@gmail.com>
2019-02-12 13:45:04 -0800

    Add edit profile sections

--- src/components/Profile/EditProfile.js
+++ src/components/Profile/EditProfile.js
@@ -7,11 +7,15 @@ import './Profile.min.css';
 
 const INITIAL_STATE = {
     role: '',
+    avatar: '',
     firstName: '',
     lastName: '',
-    location: '',
+    city: '',
+    state: '',
+    zipCode: '',
     status: '',
-    available: true
+    available: true,
+    error: ''
 };
 
 
@@ -47,35 +51,95 @@ class EditProfileBase extends Component {
                                 height="160" 
                                 className="mb-4 img-fluid img-thumbnail rounded-circle fdb-box__image" 
                                 src={user} />
-                                <h5>Change profile photo:</h5>
-                                <input type="file" name="avatar" className="fdb-box__avatar mx-auto" />
+                                <div className="form-group">
+                                  <label for="exampleFormControlFile1"><h5>Change profile photo</h5></label>
+                                  <input type="file" className="form-control-file fdb-box__avatar mx-auto" name="avatar" />
+                                </div>
                           </div>
   
-                          <div className="col col-md-8 fdb-box fdb-touch mt-5 text-center">
-                                <div className="mb-4">
-                                  <h5>Email:</h5> <span>Edit name</span>
-                                </div>
-                                <div className="mb-4">
-                                  <h5>Role:</h5> <span>Edit role</span>
-                                </div>
-                                <div className="mb-4">
-                                  <h5>Status:</h5> <em>Edit Status</em>
-                                </div>
-                          </div>
-                                
-                          <div className="col col-md-8 fdb-touch fdb-box mt-5">
-                                <div className="team-5">
-                                  <div className="row text-center justify-content-center">
-                                    <div className="col-8 mb-4">
-                                      <h4>Upload resume</h4>
+                          <div className="col col-md-8 fdb-box fdb-touch mt-5">
+                              <h4 className="text-center mb-4">Personal details</h4>
+                                  <div className="form-row">
+                                    <div className="form-group col-md-6">
+                                      <label for="user-name">First Name*</label>
+                                      <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        name="firstName" 
+                                        id="user-name" 
+                                        onChange={this.onChange} />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                      <label for="user-surname">Last Name*</label>
+                                      <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        name="lastName" 
+                                        id="user-surname" 
+                                        onChange={this.onChange} />
                                     </div>
                                   </div>
-  
-                                <div className="row text-center justify-content-center">
-                                  
-                                </div> 
-                               </div>
+
+                                  <div className="form-row">
+                                    <div className="form-group col-md-6">
+                                      <label for="inputCity">City</label>
+                                      <input type="text" name="city" className="form-control" id="inputCity" />
+                                    </div>
+                                    <div className="form-group col-md-4">
+                                      <label for="inputState">State</label>
+                                      <select id="inputState" name="state" className="form-control">
+                                        <option selected>Choose...</option>
+                                        <option>...</option>
+                                      </select>
+                                    </div>
+                                    <div className="form-group col-md-2">
+                                      <label for="inputZip">Zip</label>
+                                      <input type="text" name="zipCode" className="form-control" id="inputZip" />
+                                    </div>
+                                  </div>
                             </div>
+                                
+                          <div className="col col-md-8 fdb-touch fdb-box mt-5">
+                              <h4 className="text-center mb-4">Current status</h4>
+                              <div className="form-group">
+                                <div className="form-check form-check-inline">
+                                  <input 
+                                    className="form-check-input" 
+                                    type="radio" 
+                                    name="role" 
+                                    id="job-seeker" 
+                                    value="jobseeker"
+                                    onChange={this.onChange}
+                                    aria-describedby="roleHelp" />
+                                  <label className="form-check-label" htmlFor="job-seeker">I am a job seeker</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                  <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="role"
+                                    id="pro"
+                                    value="pro"
+                                    onChange={this.onChange} 
+                                    aria-describedby="roleHelp" />
+                                  <label className="form-check-label" htmlFor="pro">I am a professional</label>
+                                </div>
+                                <small id="roleHelp" className="form-text text-muted">*You might need to provide additional information depending on your role</small>
+                              </div>
+
+                              <div className="form-group mt-5">
+                                <label for="user-status"><h5>Shortly describe why are you here</h5></label>
+                                <textarea className="form-control" name="status" id="user-status" rows="3"></textarea>
+                              </div>
+    
+                            </div>
+                                  
+                            {/* <button 
+                              type="submit" 
+                              className="btn btn-primary fdb-box__btn"
+                              onClick={this.onSubmit}>
+                                Submit
+                            </button> */}
                         </>
                       )}
                     </AuthUserContext.Consumer>
